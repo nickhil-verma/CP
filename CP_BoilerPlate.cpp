3719bd341ad66a5db8003dc14fbcf8668395edbc
nickhil-verma <137506342+nickhil-verma@users.noreply.github.com>
2024-07-06 17:18:27 +0530

    Add files via upload

---
+++ CP_BoilerPlate.cpp
@@ -0,0 +1,28 @@
+
+#include <bits/stdc++.h>
+
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+
+void solve(){
+
+}
+
+int manin(){
+
+
+    return 0;
+}
